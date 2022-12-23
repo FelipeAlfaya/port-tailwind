20a51dc5439edcb287466db485e2583bb883446b
FelipeAlfaya <felipealfaya90@gmail.com>
2022-12-23 11:26:09 -0300

    going out, gotta add toast notifies soon to contact page

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,30 +1,27 @@
 import Head from 'next/head'
 import Header from '../components/Header'
 import Particle from '../components/Particle'
-import emailjs from '@emailjs/browser'
 import { useState } from 'react'
 import { sendContactEmail } from '../services/sendMail'
-// import { useRef } from 'react';
+import toast, { Toaster } from 'react-hot-toast'
+import 'react-toastify/dist/ReactToastify.css'
 
 function contact() {
   const [nome, setNome] = useState('')
   const [email, setEmail] = useState('')
-  const [subject, setSubject] = useState('')
   const [content, setContent] = useState('')
 
   async function handleSubmit(event: any) {
     event.preventDefault()
-    console.log(nome, email, subject, content)
+    console.log(nome, email, content)
     if (nome === '' || email === '' || content === '') {
-      alert('Please fill all the fields')
+      alert('Please fill all the fields.')
       return
     }
-
     try {
       await sendContactEmail(nome, email, content)
       setNome('')
       setEmail('')
-      setSubject('')
       setContent('')
       alert('Email sent successfully')
     } catch {
@@ -41,14 +38,16 @@ function contact() {
       <Header />
       <Particle />
       <div>
-        <h1 className='text-6xl p-2.5 m-2.5 bg-transparent font-bold text-center md:text-4xl lg:text-4xl z-50'>
+        <h1 className='text-6xl p-2.5 m-2.5 bg-transparent font-bold text-center md:text-4xl lg:text-6xl z-50'>
           Need my <span className='glow text-[#019fb6]/70 '>services</span>?
         </h1>
       </div>
+
       <div className='msg-co flex flex-col md:mt-16 sm:mt-24 container mx-auto max-w-fit max-h-fit px-4 p-4'>
         <h1 className='text-6xl p-2.5 m-2.5 sm:p-1.5 sm:m-1.5 text-[#019fb6]/70 bg-transparent font-bold text-center sm:text-2xl md:text-4xl lg:text-4xl z-50'>
           Contact me!
         </h1>
+
         <form
           data-aos='fade-up'
           onSubmit={handleSubmit}
@@ -61,6 +60,7 @@ function contact() {
             onChange={({ target }) => setNome(target.value)}
             className='p-2.5 m-2.5 bg-transparent border border-[#019fb6]'
           ></input>
+
           <input
             value={email}
             type='email'
@@ -68,12 +68,14 @@ function contact() {
             onChange={({ target }) => setEmail(target.value)}
             className='p-2.5 m-2.5 bg-transparent border border-[#019fb6]'
           ></input>
+
           <textarea
             value={content}
             placeholder='Message'
             onChange={({ target }) => setContent(target.value)}
             className='p-2.5 m-2.5 bg-transparent border border-[#019fb6]'
           ></textarea>
+
           <button
             type='submit'
             value='Send'
